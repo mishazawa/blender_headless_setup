fe717839957964380a6221b9254f438d06916e92
Misha Mishchenko <macintoshkrug@gmail.com>
2023-12-02 00:38:11 +0200

    replace raw bpy api with easybpy

--- scripts/preset_0.py
+++ scripts/preset_0.py
@@ -6,17 +6,17 @@ argv = sys.argv
 argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
 # set frame range
-bpy.data.scenes["Scene"].frame_start = int(argv[0])
-bpy.data.scenes["Scene"].frame_end = int(argv[1])
+frame_start(int(argv[0]))
+frame_end(int(argv[1]))
 
 # get pivot object for camera rotation
 pivot = get_object("Pivot")
 
 # insert initial keyframe
-start_frame = add_keyframe(pivot, "rotation_euler", bpy.data.scenes["Scene"].frame_start)
+start_frame = add_keyframe(pivot, "rotation_euler", frame_start())
 
 # do rotation
 rotate_around_z(int(argv[2]), pivot)
 
 # insert last keyframe
-end_frame = add_keyframe(pivot, "rotation_euler", bpy.data.scenes["Scene"].frame_end)
+end_frame = add_keyframe(pivot, "rotation_euler", frame_end())
